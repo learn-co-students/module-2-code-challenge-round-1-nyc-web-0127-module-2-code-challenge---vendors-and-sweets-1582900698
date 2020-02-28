74b9ccf6c6c8b8646ebfd1665c8c80786870656f
davidmlee4 <davidmlee4@gmail.com>
2020-02-28 10:51:23 -0500

    final final version

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,6 +2,10 @@ class VendorsController < ApplicationController
 
   def index
     @vendors = Vendor.all
+    total_price_of_all = VendorSweet.all.map{|vs|vs.price}.sum
+    num_of_sweets_by_all_vendors = VendorSweet.all.count 
+    avg = total_price_of_all/num_of_sweets_by_all_vendors
+     @average_price_for_all_sweets = avg.round(2)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :vendor_sweets
-  resources :sweets 
-  resources :vendors
+  resources :vendor_sweets, only: [:new, :create]
+  resources :sweets, only: [:index, :show]
+  resources :vendors, only: [:index, :show]
+  # get '/vendors', to: "vendors#index"
+  # get '/vendors_sweets/new', to: "vendors_sweets#new"
+  # post 'vendors_sweets', to: "vendor_sweets#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
