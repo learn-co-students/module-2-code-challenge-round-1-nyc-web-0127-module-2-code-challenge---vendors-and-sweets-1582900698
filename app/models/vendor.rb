2b341472647fc001be8db6d1c1e9626a9973dd31
wags83 <gpwagner@gmail.com>
2020-02-28 10:38:12 -0500

    Done with basics

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,18 @@
 class Vendor < ApplicationRecord
     has_many :vendor_sweets
     has_many :sweets, through: :vendor_sweets
+
+    # def total_price
+    #     self.sweets.reduce(0) { |sum, sweet| sum + sweet.price}
+    # end
+
+    # def num_sweets
+    #     self.sweets.length  
+    # end
+
+    # def avg_price
+    #     self.total_price / self.num_sweets
+    # end
+
+
 end
